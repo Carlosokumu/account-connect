11d09af3c739be17233603ee0c8dc7f03299432e
Carlosokumu <carlosokumu254@gmail.com>
2025-05-11 09:41:41 +0300

    define cli as the entry point to the app

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,9 @@
 package main
 
 import (
-	"account-connect/config"
-	"account-connect/internal/applications"
-	"log"
+	"account-connect/cli"
 )
 
 func main() {
-
-	cfg, err := config.LoadConfig()
-	if err != nil {
-		log.Fatalf("Failed to read app configurations %v", err)
-	}
-
-	//Pass required  arguments
-	t := applications.NewTrader("", "")
-
-	err = t.EstablishCtraderConnection(cfg)
-	if err != nil {
-		log.Fatalf("Failed to establish cTrader connection: %v", err)
-	}
-
-	if err := t.AuthorizeApplication(); err != nil {
-		log.Fatalf("Failed to authorize application: %v", err)
-	}
-
+	cli.Execute()
 }
