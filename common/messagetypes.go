7a69668b7b5c62739c36be6a5014ffb3fde8ed75
Carlosokumu <carlosokumu254@gmail.com>
2025-05-12 21:46:23 +0300

    add account historical  deals message ids

--- common/messagetypes.go
+++ common/messagetypes.go
@@ -5,4 +5,6 @@ import messages "account-connect/gen"
 var (
 	AppAuthMsgType     = uint32(messages.ProtoOAPayloadType_PROTO_OA_APPLICATION_AUTH_REQ)
 	AccountAuthMsgType = uint32(messages.ProtoOAPayloadType_PROTO_OA_ACCOUNT_AUTH_REQ)
+	HeartBeatMsgType = uint32(messages.ProtoPayloadType_HEARTBEAT_EVENT)
+	AccountHistoricalDeals = uint32(messages.ProtoOAPayloadType_PROTO_OA_DEAL_LIST_REQ)
 )
