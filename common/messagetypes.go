59ac469ac5c5e9239a926d1817c88c112f8ad496
Carlosokumu <carlosokumu254@gmail.com>
2025-05-15 22:04:01 +0300

    feat: add refreshtoken message ids

--- common/messagetypes.go
+++ common/messagetypes.go
@@ -3,8 +3,9 @@ package common
 import messages "account-connect/gen"
 
 var (
-	AppAuthMsgType     = uint32(messages.ProtoOAPayloadType_PROTO_OA_APPLICATION_AUTH_REQ)
-	AccountAuthMsgType = uint32(messages.ProtoOAPayloadType_PROTO_OA_ACCOUNT_AUTH_REQ)
-	HeartBeatMsgType = uint32(messages.ProtoPayloadType_HEARTBEAT_EVENT)
+	AppAuthMsgType         = uint32(messages.ProtoOAPayloadType_PROTO_OA_APPLICATION_AUTH_REQ)
+	AccountAuthMsgType     = uint32(messages.ProtoOAPayloadType_PROTO_OA_ACCOUNT_AUTH_REQ)
+	HeartBeatMsgType       = uint32(messages.ProtoPayloadType_HEARTBEAT_EVENT)
 	AccountHistoricalDeals = uint32(messages.ProtoOAPayloadType_PROTO_OA_DEAL_LIST_REQ)
+	RefreshTokenMsgType    = uint32(messages.ProtoOAPayloadType_PROTO_OA_REFRESH_TOKEN_REQ)
 )
