4a53d698de65b075ae3d744ff460851d436b022f
Carlosokumu <carlosokumu254@gmail.com>
2025-06-03 13:37:24 +0300

    add required messsage identifiers

--- common/messagetypes.go
+++ common/messagetypes.go
@@ -8,4 +8,6 @@ var (
 	HeartBeatMsgType       = uint32(messages.ProtoPayloadType_HEARTBEAT_EVENT)
 	AccountHistoricalDeals = uint32(messages.ProtoOAPayloadType_PROTO_OA_DEAL_LIST_REQ)
 	RefreshTokenMsgType    = uint32(messages.ProtoOAPayloadType_PROTO_OA_REFRESH_TOKEN_REQ)
+	TraderInfoMsgType      = uint32(messages.ProtoOAPayloadType_PROTO_OA_TRADER_REQ)
+	TrendBarsMsyType       = uint32(messages.ProtoOAPayloadType_PROTO_OA_GET_TRENDBARS_REQ)
 )
