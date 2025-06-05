c535b3d72b0780fbd0a6a04c1ea6f2d469c3f59c
Carlosokumu <carlosokumu254@gmail.com>
2025-06-05 13:47:25 +0300

    add symbol list res  message types

--- common/messagetypes.go
+++ common/messagetypes.go
@@ -3,11 +3,12 @@ package common
 import messages "account-connect/gen"
 
 var (
-	AppAuthMsgType         = uint32(messages.ProtoOAPayloadType_PROTO_OA_APPLICATION_AUTH_REQ)
-	AccountAuthMsgType     = uint32(messages.ProtoOAPayloadType_PROTO_OA_ACCOUNT_AUTH_REQ)
-	HeartBeatMsgType       = uint32(messages.ProtoPayloadType_HEARTBEAT_EVENT)
-	AccountHistoricalDeals = uint32(messages.ProtoOAPayloadType_PROTO_OA_DEAL_LIST_REQ)
-	RefreshTokenMsgType    = uint32(messages.ProtoOAPayloadType_PROTO_OA_REFRESH_TOKEN_REQ)
-	TraderInfoMsgType      = uint32(messages.ProtoOAPayloadType_PROTO_OA_TRADER_REQ)
-	TrendBarsMsyType       = uint32(messages.ProtoOAPayloadType_PROTO_OA_GET_TRENDBARS_REQ)
+	AppAuthMsgType           = uint32(messages.ProtoOAPayloadType_PROTO_OA_APPLICATION_AUTH_REQ)
+	AccountAuthMsgType       = uint32(messages.ProtoOAPayloadType_PROTO_OA_ACCOUNT_AUTH_REQ)
+	HeartBeatMsgType         = uint32(messages.ProtoPayloadType_HEARTBEAT_EVENT)
+	AccountHistoricalDeals   = uint32(messages.ProtoOAPayloadType_PROTO_OA_DEAL_LIST_REQ)
+	RefreshTokenMsgType      = uint32(messages.ProtoOAPayloadType_PROTO_OA_REFRESH_TOKEN_REQ)
+	TraderInfoMsgType        = uint32(messages.ProtoOAPayloadType_PROTO_OA_TRADER_REQ)
+	TrendBarsMsyType         = uint32(messages.ProtoOAPayloadType_PROTO_OA_GET_TRENDBARS_REQ)
+	AccountSymbolListMsgType = uint32(messages.ProtoOAPayloadType_PROTO_OA_SYMBOLS_LIST_REQ)
 )
