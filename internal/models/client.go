394c7f42689954d6b62804a9df60509c9616f1d9
Carlosokumu <carlosokumu254@gmail.com>
2025-07-11 23:30:26 +0300

    feat: add account-connect -client functions to add and remove streams

--- internal/models/client.go
+++ internal/models/client.go
@@ -1,12 +1,43 @@
 package models
 
 import (
+	"errors"
+	"sync"
+
 	"github.com/gorilla/websocket"
 )
+
+const StreamBufferSize = 500
 
 type AccountConnectClient struct {
 	ID           string
 	Conn         *websocket.Conn
 	PlatformConn *websocket.Conn
 	Send         chan []byte
+	Streams      map[string]chan []byte
+	StreamsMutex sync.Mutex
+}
+
+// AddStream adds a new stream to the [Streams] map
+func (c *AccountConnectClient) AddStream(streamID string) error {
+	c.StreamsMutex.Lock()
+	defer c.StreamsMutex.Unlock()
+
+	if _, exists := c.Streams[streamID]; exists {
+		return errors.New("stream already exists")
+	}
+
+	c.Streams[streamID] = make(chan []byte, StreamBufferSize)
+	return nil
+}
+
+// RemoveStream removes a stream from the [Streams] map
+func (c *AccountConnectClient) RemoveStream(streamID string) {
+	c.StreamsMutex.Lock()
+	defer c.StreamsMutex.Unlock()
+
+	if stream, exists := c.Streams[streamID]; exists {
+		close(stream)
+		delete(c.Streams, streamID)
+	}
 }
