5fade5b5362cd0408517e5596cd60fa809fa6ff7
Carlos <carlosokumu254@gmail.com>
2025-07-14 13:03:09 +0300

    feat: add  function to load required configs

--- config/config.go
+++ config/config.go
@@ -1,24 +1,31 @@
 package config
 
 import (
+	"errors"
 	"os"
 
 	"gopkg.in/yaml.v2"
+)
+
+var (
+	AccountConnectPort int32
+	CtraderPort        int32
+	CtraderEndpoint    string
 )
 
 type Config struct {
 	Servers struct {
 		Ctrader struct {
 			Endpoint string `yaml:"endpoint"`
-			Port     int32    `yaml:"port"`
+			Port     int32  `yaml:"port"`
 		} `yaml:"ctrader"`
 		AccountConnectServer struct {
-			Port int `yaml:"port"`
+			Port int32 `yaml:"port"`
 		} `yaml:"account-connect-server"`
 	} `yaml:"servers"`
 }
 
-func LoadConfig() (*Config, error) {
+func loadConfig() (*Config, error) {
 	f, err := os.Open("./config.yml")
 	if err != nil {
 		return nil, err
@@ -30,4 +37,21 @@ func LoadConfig() (*Config, error) {
 		return nil, err
 	}
 	return &cfg, nil
+}
+
+func LoadConfigs() error {
+	cfg, err := loadConfig()
+	if err != nil {
+		return err
+	}
+
+	AccountConnectPort = cfg.Servers.AccountConnectServer.Port
+	if AccountConnectPort == 0 {
+		return errors.New("Required account-connect port is missing")
+	}
+
+	CtraderPort = cfg.Servers.Ctrader.Port
+	CtraderEndpoint = cfg.Servers.Ctrader.Endpoint
+	return nil
+
 }
