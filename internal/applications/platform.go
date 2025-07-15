8f9359dbd43ad1a5e1193e7e4565bcff22822502
Carlos <carlosokumu254@gmail.com>
2025-07-15 09:00:34 +0300

    chore: rename Platform to PlatformAdapter

--- internal/applications/platform.go
+++ internal/applications/platform.go
@@ -21,9 +21,9 @@ type PlatformConnectionStatus struct {
 	Authorized bool
 }
 
-// Platform defines a set of method signatures that are common for all trading platforms and that are required by
+// Platform defines a set of method signatures that are common for all trading platforms APIs and that are required by
 // account-connect suported functionality
-type Platform interface {
+type PlatformAdapter interface {
 	EstablishConnection(ctxt context.Context, cfg PlatformConfigs) error
 	GetHistoricalTrades(ctx context.Context, payload messages.AccountConnectHistoricalDealsPayload) error
 	GetTraderInfo(ctx context.Context, payload messages.AccountConnectTraderInfoPayload) error
